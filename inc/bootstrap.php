3ab78c71b651d85f96cb04c4e900e3796af90284
Melinda S <roseofgold@gmail.com>
2019-06-05 19:51:14 -0700

    add user functions to bootstrap

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/functions_tasks.php';
+require_once __DIR__ . '/functions_users.php';
 require_once __DIR__ . '/functions_auth.php';
 
 try {
