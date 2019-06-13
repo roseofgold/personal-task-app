674ae8d11372d95ec939eb5080496bc6cc513338
Melinda S <roseofgold@gmail.com>
2019-06-12 19:09:17 -0700

    add comments

--- inc/actions_tasks.php
+++ inc/actions_tasks.php
@@ -13,12 +13,14 @@ if (request()->get('filter')) {
 }
 
 switch ($action) {
-    // add a task to a specific user
+    // Add a task to a specific user
     case "add":
+        // Check to see is task has been entered.
         if (empty($task)) {
             $session->getFlashBag()->add('error', 'Please enter a task');
             redirect('/task.php');
         } else {
+            // Create task in database. If errors, state so.
             if (createTask(['task'=>$task, 'status'=>$status, 'user_id'=>$user_id])) {
                 $session->getFlashBag()->add('success', 'New Task Added for '. $user_id);
             } else {
@@ -27,24 +29,24 @@ switch ($action) {
         }
         break;
 
-    // update a task
+    // Update a task
     case "update":
         $data = ['task_id'=>$task_id, 'task'=>$task, 'status'=>$status];
         if (updateTask($data)) {
             $session->getFlashBag()->add('success', 'Task Updated');
         } else {
-            $session->getFlashBag()->add('error', 'Could NOT update task');
+            $session->getFlashBag()->add('error', 'Could NOT Update Task');
         }
         break;
     
-    // change status of task
+    // Change status of task
     case "status":
         if (updateStatus(['task_id'=>$task_id, 'status'=>$status])) {
             if ($status == 1) $session->getFlashBag()->add('success', 'Task Complete');
         }
         break;
     
-    // remove task from list
+    // Remove task from list
     case "delete":
         if (deleteTask($task_id)) {
             $session->getFlashBag()->add('success', 'Task Deleted');
