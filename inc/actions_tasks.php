735edc448b5898f0af9ade7bfa87561ea9a64722
Melinda S <roseofgold@gmail.com>
2019-06-12 19:05:17 -0700

    add comments

--- inc/actions_tasks.php
+++ inc/actions_tasks.php
@@ -13,36 +13,44 @@ if (request()->get('filter')) {
 }
 
 switch ($action) {
-case "add":
-    if (empty($task)) {
-        $session->getFlashBag()->add('error', 'Please enter a task');
-        redirect('/task.php');
-    } else {
-        if (createTask(['task'=>$task, 'status'=>$status, 'user_id'=>$user_id])) {
-            $session->getFlashBag()->add('success', 'New Task Added for '. $user_id);
+    // add a task to a specific user
+    case "add":
+        if (empty($task)) {
+            $session->getFlashBag()->add('error', 'Please enter a task');
+            redirect('/task.php');
+        } else {
+            if (createTask(['task'=>$task, 'status'=>$status, 'user_id'=>$user_id])) {
+                $session->getFlashBag()->add('success', 'New Task Added for '. $user_id);
+            } else {
+                $session->getFlashBag()->add('error', 'Task Not Added');
+            }
         }
-        $session->getFlashBag()->add('error', 'Task Not Added');
-    }
-    break;
-case "update":
-    $data = ['task_id'=>$task_id, 'task'=>$task, 'status'=>$status];
-    if (updateTask($data)) {
-        $session->getFlashBag()->add('success', 'Task Updated');
-    } else {
-        $session->getFlashBag()->add('error', 'Could NOT update task');
-    }
-    break;
-case "status":
-    if (updateStatus(['task_id'=>$task_id, 'status'=>$status])) {
-        if ($status == 1) $session->getFlashBag()->add('success', 'Task Complete');
-    }
-    break;
-case "delete":
-    if (deleteTask($task_id)) {
-        $session->getFlashBag()->add('success', 'Task Deleted');
-    } else {
-        $session->getFlashBag()->add('error', 'Could NOT Delete Task');
-    }
-    break;
+        break;
+
+    // update a task
+    case "update":
+        $data = ['task_id'=>$task_id, 'task'=>$task, 'status'=>$status];
+        if (updateTask($data)) {
+            $session->getFlashBag()->add('success', 'Task Updated');
+        } else {
+            $session->getFlashBag()->add('error', 'Could NOT update task');
+        }
+        break;
+    
+    // change status of task
+    case "status":
+        if (updateStatus(['task_id'=>$task_id, 'status'=>$status])) {
+            if ($status == 1) $session->getFlashBag()->add('success', 'Task Complete');
+        }
+        break;
+    
+    // remove task from list
+    case "delete":
+        if (deleteTask($task_id)) {
+            $session->getFlashBag()->add('success', 'Task Deleted');
+        } else {
+            $session->getFlashBag()->add('error', 'Could NOT Delete Task');
+        }
+        break;
 }
 header("Location: ".$url);
